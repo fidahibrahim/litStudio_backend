d69df223f7cef062f3ff86a5fa8e22e182d2ed48
fidahibrahim <147613406+fidahibrahim@users.noreply.github.com>
2025-05-06 02:51:19 +0530

    crud completed

--- dist/server.js
+++ dist/server.js
@@ -16,7 +16,7 @@ dotenv_1.default.config();
 app.use((0, cookie_parser_1.default)());
 app.use((0, morgan_1.default)("dev"));
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
+    origin: 'https://lit-studio-frontend-ips8.vercel.app',
     credentials: true
 }));
 app.use(express_1.default.json());
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(morgan("dev"))
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://lit-studio-frontend-ips8.vercel.app',
     credentials: true
 }))
 
